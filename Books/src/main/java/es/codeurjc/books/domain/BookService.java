322ce9c94c9e2825bb8cea399eb6437f939fded4
Enrique Casas Fernández <enrique.casas@nfq.es>
2021-02-22 02:25:36 +0100

    Transición a nueva arquitectura

---
+++ Books/src/main/java/es/codeurjc/books/domain/BookService.java
@@ -0,0 +1,6 @@
+package es.codeurjc.books.domain;
+
+public interface BookService {
+
+	public void createBook(BookDTO book);
+}
---
+++ Books/src/main/java/es/codeurjc/books/domain/Comment.java
@@ -0,0 +1,13 @@
+package es.codeurjc.books.domain;
+
+public class Comment {
+	private Long id;
+
+	private String publishtext;
+
+	private int punctuation;
+
+	private Book book;
+
+	private User user;
+}
